4daa1a58edd7bbbd6c367d95fb20f0b92dcd3112
imed <retibi_i@etna-alternance.net>
2023-06-05 02:04:06 +0200

    ll

--- src/database/instance.ts
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const Sequelize = require('sequelize');
-
-const seq = new Sequelize(process.env.DB_NAME, process.env.USER_DB, process.env.PASSWORD_DB, {  // create a new instance of sequelize
-  host: 'localhost',
-  dialect: 'mysql',
-});
-export default seq;
